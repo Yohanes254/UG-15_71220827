7d75fc7ea2bc4863886ce79f6bb357ef20ecf06c
Yohanes254 <117884349+Yohanes254@users.noreply.github.com>
2022-12-15 12:57:59 +0700

    Add files via upload

---
+++ soal no 1.py
@@ -0,0 +1,34 @@
+import turtle
+t =turtle.Turtle()
+#Alphabet Y
+t.up()
+t.goto(-200,50)
+t.pensize(10)
+t.down()
+t.left(60)
+t.forward(100)
+
+t.back(50)
+t.left(60)
+t.forward(50)
+
+#8
+#2
+#7
+# Alpahabet F
+t.penup()
+t.setpos(-50,50)
+t.pensize(40)
+
+t.pendown()
+t.forward(100)
+t.backward(100)
+t.right(90)
+t.forward(100)
+
+t.left(90)
+t.forward(100)
+t.backward(100)
+
+t.right(90)
+t.backward(100)
